4e36fe054968bf3758f0be4a6e0835edcb89d944
Jack Storror <jackstorror@gmail.com>
2021-10-19 14:44:36 +0100

    Top up functionality added, Two Rspec tests added. 0 failures

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,7 +1,7 @@
 require 'oystercard'
 describe Oystercard do
   let(:oystercard) {Oystercard.new}
-  context 'Oystercard initialisation tests' do
+  context 'initialisation tests' do
     it 'class can be called and created' do
       expect(oystercard).to eq oystercard
     end
@@ -9,8 +9,16 @@ describe Oystercard do
       expect(subject.balance).to eq(0)
   end
   context '#top_up' do
+    let(:maximum_balance) {maximum_balance = Oystercard::MAXIMUM_BALANCE}
     it 'increases the balance' do
       expect{ subject.top_up(5) }.to change{ subject.balance }.by(5) 
+    end
+    it 'Deposits of more than £90 raise error' do
+      expect{ subject.top_up(91) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+    end
+    it 'raises an error is the maximum balance is exceeded' do
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up(5) }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
     end
   end
 end
